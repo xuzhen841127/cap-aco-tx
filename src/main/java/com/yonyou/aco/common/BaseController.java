5e7f75e5b244c9fb082a1c7a23a4f420a8938019
xuzhen <5xuzhen@163.com>
2019-09-08 00:09:48 +0800

    修改注释

--- src/main/java/com/yonyou/aco/common/BaseController.java
+++ src/main/java/com/yonyou/aco/common/BaseController.java
@@ -5,6 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.yonyou.aco.plan.service.IPlanService;
 import com.yonyou.aco.realpay.service.IRealpayService;
 
+/**
+ * <p> 概述：Controller基类
+ * <p> 功能：Controller基类
+ * <p> 作者：徐真
+ * <p> 创建时间：2019年9月7日
+ * <p> 类调用特殊情况：无
+ */
 public class BaseController {
 	@Autowired
 	public IPlanService planService;
